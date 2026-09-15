b6e39dc17b395669f760a4439ad3b2cc38b41629
Your Name <luke_christoffersen@mines.edu>
2026-09-15 15:48:59 -0600

    feat: added Setting enum file, should be complete

---
+++ src/Setting.java
@@ -0,0 +1,15 @@
+
+public enum Setting {
+	OFF("---"), LOW("--+"), MEDIUM("-++"), HIGH("+++");
+
+	private String _setting;
+
+	Setting(String setting) {
+		this._setting = setting;
+	}
+
+	@Override
+	public String toString() {
+		return _setting;
+	}
+}
